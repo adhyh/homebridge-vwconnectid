378afd2f962cf7c596ff63f7fb613c93cf8d09f6
Adhy <20784602+adhyh@users.noreply.github.com>
2023-02-06 16:59:24 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'WeConnectID';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-weconnectid';
+export const PLUGIN_NAME = 'homebridge-vwconnectid';
