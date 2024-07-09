60b336316839f939933dfcef0f5eabd7e159cc2f
Adhy <20784602+adhyh@users.noreply.github.com>
2024-07-09 09:56:46 +0200

    Update smartChargingAccessory.ts

--- src/smartChargingAccessory.ts
+++ src/smartChargingAccessory.ts
@@ -98,7 +98,7 @@ export class SmartChargingAccessory {
             return;
           } else if (data.minAvg > maxDeliveryTreshold && isCharging) {
             this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-            this.platform.log.info('redelivery > maxDeliveryTreshold, currently charging, so stop charging');
+            this.platform.log.info('minute average power > maxDeliveryTreshold, currently charging, so stop charging');
             this.platform.vwConn.stopCharging();
           }
 
