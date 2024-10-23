829b7970a80792a9d4b0fd3b708f74395f2ea927
Adhy <20784602+adhyh@users.noreply.github.com>
2024-10-23 17:29:16 +0200

    Update smartChargingAccessory.ts

--- src/smartChargingAccessory.ts
+++ src/smartChargingAccessory.ts
@@ -4,132 +4,139 @@ import { WeConnectIDPlatform } from './platform';
 import axios from 'axios';
 
 export class SmartChargingAccessory {
-  private service: Service;
+    private service: Service;
 
-  constructor(
+    constructor(
         private readonly platform: WeConnectIDPlatform,
         private readonly accessory: PlatformAccessory,
         private intervalId: NodeJS.Timer | null = null,
-  ) {
+    ) {
 
         this.accessory.getService(this.platform.Service.AccessoryInformation)!
-          .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Volkswagen')
-          .setCharacteristic(this.platform.Characteristic.Model, this.platform.vwConn.vehicles.data.find(({ vin }) =>
-            vin === this.platform.config.weconnect.vin).model)
-          .setCharacteristic(this.platform.Characteristic.SerialNumber, this.platform.config.weconnect.vin);
+            .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Volkswagen')
+            .setCharacteristic(this.platform.Characteristic.Model, this.platform.vwConn.vehicles.data.find(({ vin }) =>
+                vin === this.platform.config.weconnect.vin).model)
+            .setCharacteristic(this.platform.Characteristic.SerialNumber, this.platform.config.weconnect.vin);
 
         this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
 
         this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.displayName);
 
         this.service.getCharacteristic(this.platform.Characteristic.On)
-          .onSet(this.setOn.bind(this))
-          .onGet(this.getOn.bind(this));
-  }
-
-  async fetchJSONData(url: string): Promise<any> {
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      this.platform.log.info('Error fetching JSON data:', error);
-      throw error;
-    }
-  }
-
-  async smartChargingLoop() {
-
-    const range = this.platform.vwConn.idData.charging.batteryStatus.value.cruisingRangeElectric_km;
-    const isCharging = this.platform.vwConn.idData.charging.chargingStatus.value.chargingState === 'charging';
-    const isReadyForCharging = this.platform.vwConn.idData.charging.chargingStatus.value.chargingState === 'readyForCharging';
-    const isReduced = this.platform.vwConn.idData.charging.chargingSettings.value.maxChargeCurrentAC === 'reduced';
-    const highTariffKmTreshold = this.accessory.context.device.highTariffKmTreshold;
-    const lowTariffKmTreshold = this.accessory.context.device.lowTariffKmTreshold;
-    const minRedeliveryTreshold = -1 * this.accessory.context.device.minRedeliveryTreshold;
-    const maxDeliveryTreshold = this.accessory.context.device.maxDeliveryTreshold;
-
-    if (range < highTariffKmTreshold) {
-      if (isReduced) {
-        this.platform.log.info('range below highTariffKmTreshold, setting max charge current');
-        this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
-      }
-      if (isReadyForCharging) {
-        this.platform.log.info('range below highTariffKmTreshold, start charging');
-        this.platform.vwConn.startCharging();
-      }
-    } else {
-      const url = this.accessory.context.device.energyDataSource;
-      this.fetchJSONData(url)
-        .then((data) => {
-
-          if (range < lowTariffKmTreshold && data.lowTariff) {
-            if (isReduced) {
-              this.platform.log.info('range below lowTariffKmTreshold, setting maximum charge current');
-              this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
-            }
-                
-            if (isReadyForCharging) {
-              this.platform.log.info('range below lowTariffKmTreshold and low tariff, start charging');
-              this.platform.vwConn.startCharging();
-            }
-          } else if (data.minAvg < minRedeliveryTreshold) {
-
-            if (isCharging && isReduced) {
-              this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-              this.platform.log.info('minute average power < minRedeliveryTreshold, currently charging, so setting maximum charge current');
-              this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
-              return;
-            }
-            if (!isReduced) {
-              this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-              this.platform.log.info('minute average power < minRedeliveryTreshold, currently maximum charge current, setting to reduced');
-              this.platform.vwConn.setChargingSetting('chargeCurrent', 'reduced');
-            }
-            if (isReadyForCharging) {
-              this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-              this.platform.log.info('minute average power < minRedeliveryTreshold, start charging');
-              this.platform.vwConn.startCharging();
-            }
-
-          } else if (data.minAvg > maxDeliveryTreshold && !isReduced && isCharging) {
-            this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-            this.platform.log.info('minute average power > maxDeliveryTreshold, currently charging at max, so setting to reduced');
-            this.platform.vwConn.setChargingSetting('chargeCurrent', 'reduced');
-            return;
-          } else if (data.minAvg > maxDeliveryTreshold && isCharging) {
-            this.platform.log.info('JSON Data minAvg: ', data.minAvg);
-            this.platform.log.info('minute average power > maxDeliveryTreshold, currently charging, so stop charging');
-            this.platform.vwConn.stopCharging();
-          }
-
-        })
-        .catch((error) => {
-          this.platform.log.error('Error in fetching energy data: ', error);
-        });
-    }
-  }
-
-  async setOn(value: CharacteristicValue) {
-    if (value as boolean) {
-
-      this.intervalId = setInterval(() => {
-        this.smartChargingLoop();
-      }, 60000);
-
-    } else {
-
-      if (this.intervalId) {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-      }
-
+            .onSet(this.setOn.bind(this))
+            .onGet(this.getOn.bind(this));
     }
 
-    this.platform.log.info('Set smart charging Characteristic On ->', value);
-  }
+    async fetchJSONData(url: string): Promise<any> {
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            this.platform.log.info('Error fetching JSON data:', error);
+            throw error;
+        }
+    }
 
-  async getOn(): Promise<CharacteristicValue> {
-    
-    return (this.intervalId != null);
-  }
+    async smartChargingLoop() {
+
+
+
+        try {
+            const range = this.platform.vwConn.idData?.charging?.batteryStatus?.value?.cruisingRangeElectric_km;
+            const isCharging = this.platform.vwConn.idData?.charging?.chargingStatus?.value?.chargingState === 'charging';
+            const isReadyForCharging = this.platform.vwConn.idData?.charging?.chargingStatus?.value?.chargingState === 'readyForCharging';
+            const isReduced = this.platform.vwConn.idData?.charging?.chargingSettings?.value?.maxChargeCurrentAC === 'reduced';
+            const highTariffKmTreshold = this.accessory?.context?.device?.highTariffKmTreshold;
+            const lowTariffKmTreshold = this.accessory?.context?.device?.lowTariffKmTreshold;
+            const minRedeliveryTreshold = -1 * this.accessory?.context?.device?.minRedeliveryTreshold;
+            const maxDeliveryTreshold = this.accessory?.context?.device?.maxDeliveryTreshold;
+
+            if (range < highTariffKmTreshold) {
+                if (isReduced) {
+                    this.platform.log.info('range below highTariffKmTreshold, setting max charge current');
+                    this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
+                }
+                if (isReadyForCharging) {
+                    this.platform.log.info('range below highTariffKmTreshold, start charging');
+                    this.platform.vwConn.startCharging();
+                }
+            } else {
+                const url = this.accessory.context.device.energyDataSource;
+                this.fetchJSONData(url)
+                    .then((data) => {
+
+                        if (range < lowTariffKmTreshold && data.lowTariff) {
+                            if (isReduced) {
+                                this.platform.log.info('range below lowTariffKmTreshold, setting maximum charge current');
+                                this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
+                            }
+
+                            if (isReadyForCharging) {
+                                this.platform.log.info('range below lowTariffKmTreshold and low tariff, start charging');
+                                this.platform.vwConn.startCharging();
+                            }
+                        } else if (data.minAvg < minRedeliveryTreshold) {
+
+                            if (isCharging && isReduced) {
+                                this.platform.log.info('JSON Data minAvg: ', data.minAvg);
+                                this.platform.log.info('minute average power < minRedeliveryTreshold, currently charging, so setting maximum charge current');
+                                this.platform.vwConn.setChargingSetting('chargeCurrent', 'maximum');
+                                return;
+                            }
+                            if (!isReduced) {
+                                this.platform.log.info('JSON Data minAvg: ', data.minAvg);
+                                this.platform.log.info('minute average power < minRedeliveryTreshold, currently maximum charge current, setting to reduced');
+                                this.platform.vwConn.setChargingSetting('chargeCurrent', 'reduced');
+                            }
+                            if (isReadyForCharging) {
+                                this.platform.log.info('JSON Data minAvg: ', data.minAvg);
+                                this.platform.log.info('minute average power < minRedeliveryTreshold, start charging');
+                                this.platform.vwConn.startCharging();
+                            }
+
+                        } else if (data.minAvg > maxDeliveryTreshold && !isReduced && isCharging) {
+                            this.platform.log.info('JSON Data minAvg: ', data.minAvg);
+                            this.platform.log.info('minute average power > maxDeliveryTreshold, currently charging at max, so setting to reduced');
+                            this.platform.vwConn.setChargingSetting('chargeCurrent', 'reduced');
+                            return;
+                        } else if (data.minAvg > maxDeliveryTreshold && isCharging) {
+                            this.platform.log.info('JSON Data minAvg: ', data.minAvg);
+                            this.platform.log.info('minute average power > maxDeliveryTreshold, currently charging, so stop charging');
+                            this.platform.vwConn.stopCharging();
+                        }
+
+                    })
+                    .catch((error) => {
+                        this.platform.log.error('Error in fetching energy data: ', error);
+                    });
+            }
+        } catch (error) {
+            console.error("Error in smartChargingLoop:", error);
+            // Handle the error (e.g., logging, fallback values, etc.)
+        }
+    }
+
+    async setOn(value: CharacteristicValue) {
+        if (value as boolean) {
+
+            this.intervalId = setInterval(() => {
+                this.smartChargingLoop();
+            }, 60000);
+
+        } else {
+
+            if (this.intervalId) {
+                clearInterval(this.intervalId);
+                this.intervalId = null;
+            }
+
+        }
+
+        this.platform.log.info('Set smart charging Characteristic On ->', value);
+    }
+
+    async getOn(): Promise<CharacteristicValue> {
+
+        return (this.intervalId != null);
+    }
 }
